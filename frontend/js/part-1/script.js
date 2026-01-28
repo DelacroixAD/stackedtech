6d88dc85daecedfd21a384cd33319f50c6c13bd4
Aditya Singh Rawat <adirajan29@gmail.com>
2026-01-28 23:54:22 +0530

    js commands

---
+++ frontend/js/part-1/script.js
@@ -0,0 +1,14 @@
+console.log('HELLO')
+console.warn('warn')
+console.error('error')
+var b=10
+b=12
+var c=5
+console.log(b%c)
+var a=confirm('yes')
+console.log(a)
+var d=prompt('ENTER NAME')
+console.log(d)
+alert('woah')
+confirm('yoo')
+prompt("name")
