4fab18936ce7525ace78b942ee40b39d90869b6c
Aditya Singh Rawat <adirajan29@gmail.com>
2026-01-29 23:25:02 +0530

    switch js

---
+++ frontend/js/part-2/switch.js
@@ -0,0 +1,11 @@
+switch("harsh"){
+    case 'harshit':
+        console.log ("YOU AR HARSHIT") 
+    break;
+case"harsh":
+console.log ("you are harsh")
+break;
+default:
+    console.log("you are none of them") 
+    break;
+}
