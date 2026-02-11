722022a7759d249dccbd7cb92262ea8c32e5b5ca
Aditya Singh Rawat <adirajan29@gmail.com>
2026-02-11 23:15:18 +0530

    adv js problems

--- frontend/js/part-8/script.js
+++ frontend/js/part-8/script.js
@@ -13,3 +13,36 @@ function repeatfunction (fn,time) {
     }
     var greet = greetuser("Hello");
     greet("Alice");
+    // q-3
+function onlyOncecaller(fn) {
+    let called = false;
+    return function() {
+        if (!called) {
+            called = true;
+           fn()
+        }
+        else {
+            console.log("Function has already been called once.");
+        }
+    };
+
+}
+
+var newfnc = onlyOncecaller(function(){
+    console.log("This function can only be called once.");
+});
+//q-4
+function throt(fn,delay) {
+    let lastCall = 0;
+    return function() {
+        let current = Date.now();
+        if (current- lastCall >= delay) {
+            lastCall = current;
+            fn();
+        }
+    };
+}
+var newfnc=throt(function() {
+    console.log("Function executed after delay");
+},2000)
+newfnc();
